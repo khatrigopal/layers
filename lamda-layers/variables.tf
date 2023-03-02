15aa277ca3bf746e9a00eb025bc8fedcfd1d5d47
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 20:55:53 +0530

    Create variables.tf

---
+++ lamda-layers/variables.tf
@@ -0,0 +1,23 @@
+variable "layer_name" {
+  type = string
+}
+
+variable "layer_description" {
+  type = string
+}
+
+variable "layer_code_key" {
+  type = string
+}
+
+variable "layer_code_path" {
+  type = string
+}
+
+variable "layer_code_path" {
+  type = string
+}
+
+variable "s3_bucket" {
+  type = string
+}
