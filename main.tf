d7940d8a0df366849d280497603db6a5f45d9b34
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 19:39:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,18 @@
-module "lambda_layers" {
+
+module "layer_1" {
   source = "./lambda-layers"
 
-  layers = [
-    {
-      layer_name         = "my_layer_1"
-      compatible_runtimes = ["nodejs12.x"]
-      s3_bucket          = "bskhatrig-testing"
-      s3_key             = "layers/my_layer_1.zip"
-    },
-    {
-      layer_name         = "my_layer_2"
-      compatible_runtimes = ["nodejs12.x", "python3.8"]
-      s3_bucket          = "bskhatrig-testing"
-      s3_key             = "layers/my_layer_2.zip"
-    }
-  ]
+  layer_name        = "my-layer-1"
+  layer_description = "My first Lambda layer"
+  layer_bucket      = "bskhatrig-testing"
+  layer_key         = "my-layer-1.zip"
 }
 
-output "layer_1_arn" {
-  value = module.lambda_layers.layers[0].arn
-}
-
-output "layer_2_arn" {
-  value = module.lambda_layers.layers[1].arn
+module "layer_2" {
+  source = "./lambda-layers"
+  
+  layer_name        = "my-layer-2"
+  layer_description = "My second Lambda layer"
+  layer_bucket      = "bskhatrig-testingt"
+  layer_key         = "my-layer-2.zip"
 }
