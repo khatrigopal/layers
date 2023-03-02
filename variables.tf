0f16fb66e78712dea635c25b70e2b85209d28de5
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 19:29:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -61,3 +61,14 @@ variable "test_name" {
   type        = string
   default     = "khatrig"
 }
+
+#####
+
+variable "layers" {
+  type = list(object({
+    layer_name         = string
+    compatible_runtimes = list(string)
+    s3_bucket          = string
+    s3_key             = string
+  }))
+}
