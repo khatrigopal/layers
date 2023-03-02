a4fee3bbb01242c52705ba6134d944199c550fc5
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 20:20:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "layer_1" {
   layer_name        = "my-layer-1"
   layer_description = "My first Lambda layer"
   layer_bucket      = "bskhatrig-testing"
-  layer_key         = "my-layer-1.zip"
+  layer_key         = "layers/my-layer-1.zip"
 }
 
 module "layer_2" {
@@ -14,5 +14,5 @@ module "layer_2" {
   layer_name        = "my-layer-2"
   layer_description = "My second Lambda layer"
   layer_bucket      = "bskhatrig-testing"
-  layer_key         = "my-layer-2.zip"
+  layer_key         = "layers/my-layer-2.zip"
 }
