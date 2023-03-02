438ae9d1fc0c612fd228c7d82655500daf6dc877
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 17:02:02 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "layers" {
-  type = list(object({
-    layer_name         = string
-    compatible_runtimes = list(string)
-    s3_bucket          = string
-    s3_key             = string
-  }))
-}
