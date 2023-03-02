989a23d3668da87939ef160a5396af95e24481a3
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 19:53:27 +0530

    Update main.tf

--- lamda-layers/main.tf
+++ lamda-layers/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_layer_version" "this" {
   filename   = var.layer_key
   layer_name = var.layer_name
   description = var.layer_description
-  source_code_hash = filebase64sha256(var.layer_key)
+  #source_code_hash = filebase64sha256(var.layer_key)
   compatible_runtimes = ["python3.8"]
   
   s3_bucket = var.layer_bucket
