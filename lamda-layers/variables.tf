a4191d896ec97d1d2ac88a21dc8106cb049dfeff
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-02 20:26:27 +0530

    Update variables.tf

--- lamda-layers/variables.tf
+++ lamda-layers/variables.tf
@@ -1,26 +1,20 @@
-variable "function_name" {
-  type = string
+variable "function_names" {
+  type    = list(string)
 }
 
-##Define a variable for the S3 bucket and key where your Lambda function code resides. For example:
+variable "function_runtimes" {
+  type    = list(string)
+}
 
 variable "function_code_s3_bucket" {
-  type = string
+  type    = string
 }
 
 variable "function_code_s3_key" {
-  type = string
-}
-
-
-###Define a variable for the runtime of your Lambda function. For example:
-
-variable "function_runtime" {
-  type = string
+  type    = string
 }
 
 variable "function_layers" {
-  type = list
+  type    = list(list(string))
   default = []
 }
-  
